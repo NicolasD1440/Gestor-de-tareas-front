d48c2b68f4c59784477e46863a5ea9c6a81fc942
NicolasD1440 <nicolascastiblanco6@gmail.com>
2026-06-25 18:25:42 -0500

    segundo cargue

---
+++ gestor-tareas/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,33 @@
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="Nav-main">
+      <div className="search">
+        <div className="search-container">
+          <svg 
+            className="search-icon" 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            stroke="currentColor" 
+            width="20" 
+            height="20"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+          </svg>
+          
+          <input type="text" placeholder="Search" className="search-input" />
+        </div>
+      </div>
+
+      <div className="options">
+        <i class="fa-regular fa-bell"></i>
+        <i class="fa-regular fa-circle-question"></i>
+        <i class="fa-regular fa-circle-user"></i>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
--- gestor-tareas/src/pages/home/Home.jsx
+++ gestor-tareas/src/pages/home/Home.jsx
@@ -1,14 +1,17 @@
 import Board from "../../components/Board/Board";
 import Sidebar from "../../components/Sidebar/sidebar";
+import NavBar from "../../components/NavBar/NavBar";
 import './Home.css'
 
 function Home() {
 
     return (
          <div className="Home">
+            
             <Sidebar />
             <main className="MainContent">
-                <h1>Gestor de tareas</h1>
+                <NavBar/>
+                <h3>Gestor de tareas</h3>
                 <Board />
             </main>
         </div>
